1bfb9faec851550f72235ea9b607e8f135cc7428
millecker <martin.illecker@gmail.com>
2012-07-28 11:13:32 +0000

    Updated /pipes/wordcount example
    git-svn-id: https://svn.illecker.at/hadoop/applications@8 f81d905f-b3a1-49e4-92e0-86bcef002278

--- pipes/wordcount/wordcount.sh
+++ pipes/wordcount/wordcount.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+#
+#if [ $# -ne 1 ]; then
+#  echo "specify input file." 
+#  exit 1
+#fi
 
-if [ $# -ne 1 ]; then
-  echo "specify input file." 
-  exit 1
-fi
+#$1
 
-hadoop dfs -rmr output
+hadoop dfs -rmr output/cpu-wordcount
 
-hadoop pipes -conf wordcount_job.xml -input $1 -output output
+hadoop dfs -rmr bin/cpu-wordcount
+hadoop dfs -put cpu-wordcount/cpu-wordcount bin/cpu-wordcount
+
+hadoop dfs -rmr input/cpu-wordcount
+hadoop dfs -mkdir input/cpu-wordcount
+hadoop dfs -put input/* input/cpu-wordcount
+
+hadoop pipes -conf wordcount_job.xml -input input/cpu-wordcount -output output/cpu-wordcount
