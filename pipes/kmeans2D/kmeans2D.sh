b0825f00477c325f4d087f12eced286cb5c5adaa
millecker <martin.illecker@gmail.com>
2012-07-29 08:39:36 +0000

    Kmeans2D updated - run script - job definition xml
    git-svn-id: https://svn.illecker.at/hadoop/applications@9 f81d905f-b3a1-49e4-92e0-86bcef002278

--- pipes/kmeans2D/kmeans2D.sh
+++ pipes/kmeans2D/kmeans2D.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+#
+#if [ $# -ne 1 ]; then
+#  echo "specify input file." 
+#  exit 1
+#fi
 
-if [ $# -ne 1 ]; then
-  echo "specify input file." 
-  exit 1
-fi
+#$1
 
-hadoop dfs -rmr output
+hadoop dfs -rmr output/kmeans2D
 
-hadoop pipes -D hadoop.pipes.java.recordreader=true\
- -D hadoop.pipes.java.recordwriter=true\
- -output output\
- -cpubin bin/cpu-kmeans2D\
- -gpubin bin/cpu-kmeans2D\
- -input $1
+hadoop dfs -rmr bin/kmeans2D
+hadoop dfs -put cpu-kmeans2D/cpu-kmeans2D bin/kmeans2D/cpu-kmeans2D
+hadoop dfs -put gpu-kmeans2D/gpu-kmeans2D bin/kmeans2D/gpu-kmeans2D
+
+hadoop dfs -rmr input/kmeans2D
+hadoop dfs -mkdir input/kmeans2D
+hadoop dfs -put kmeans-input2D/ik2_128_512_10 input/kmeans2D
+
+hadoop pipes -conf kmeans2D_job.xml -D mapred.child.env="LD_LIBRARY_PATH=$LD_LIBRARY_PATH" -input input/kmeans2D -output output/kmeans2D
+
