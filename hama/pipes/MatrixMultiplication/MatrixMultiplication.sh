8bae997e53fd22b4b3fce672d9dcaa508649ed0d
millecker <martin.illecker@gmail.com>
2012-10-10 10:15:26 +0000

    Fixed cpu-MatrixMultiplication: is a directory
    git-svn-id: https://svn.illecker.at/hadoop/applications@39 f81d905f-b3a1-49e4-92e0-86bcef002278

--- hama/pipes/MatrixMultiplication/MatrixMultiplication.sh
+++ hama/pipes/MatrixMultiplication/MatrixMultiplication.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 hadoop dfs -rmr output/pipes/matrixmult
-
 hadoop dfs -rmr bin/cpu-MatrixMultiplication
-hadoop dfs -put cpu-MatrixMultiplication/MatrixMultiplication bin/cpu-MatrixMultiplication
+
+hadoop dfs -put cpu-MatrixMultiplication/MatrixMultiplication bin/cpu_MatrixMultiplication
 
 # -libjars added for SequenceFile: java.io.IOException: WritableName can't load class: de.jungblut.writable.VectorWritable
 hama pipes -libjars ../../Examples/Examples.jar -conf MatrixMultiplication_job.xml -output output/pipes/matrixmult
