54e89b0d989acb82a40be45bb78d098efa69c0c4
millecker <martin.illecker@gmail.com>
2013-05-19 21:38:46 +0000

    Added rootbeer library Modified Examples/.classpath and build.sh script
    git-svn-id: https://svn.illecker.at/hadoop/applications@52 f81d905f-b3a1-49e4-92e0-86bcef002278

--- hama/Examples/build.sh
+++ hama/Examples/build.sh
@@ -11,14 +11,16 @@ LIBS=../../lib
 
 # Pack jar including all lib jars
 java -jar pack.jar -mainjar HamaExamples_tmp1.jar -libjar $LIBS/hama-core-0.7.0-SNAPSHOT.jar -destjar HamaExamples_tmp2.jar
-java -jar pack.jar -mainjar HamaExamples_tmp2.jar -libjar $LIBS/commons-logging-1.1.1.jar -destjar HamaExamples_tmp3.jar
-java -jar pack.jar -mainjar HamaExamples_tmp3.jar -libjar $LIBS/tjungblut-math-1.0.jar -destjar HamaExamples_tmp4.jar
+java -jar pack.jar -mainjar HamaExamples_tmp2.jar -libjar $LIBS/hadoop-core-1.3.0-SNAPSHOT.jar -destjar HamaExamples_tmp3.jar
+java -jar pack.jar -mainjar HamaExamples_tmp3.jar -libjar $LIBS/commons-logging-1.1.1.jar -destjar HamaExamples_tmp4.jar
+java -jar pack.jar -mainjar HamaExamples_tmp4.jar -libjar $LIBS/tjungblut-math-1.0.jar -destjar HamaExamples_tmp5.jar
 
 rm HamaExamples_tmp1.jar
 rm HamaExamples_tmp2.jar
 rm HamaExamples_tmp3.jar
+rm HamaExamples_tmp4.jar
 
-mv HamaExamples_tmp4.jar HamaExamples.jar
+mv HamaExamples_tmp5.jar HamaExamples.jar
 
 if [ "$1" == "gpu" ]; then
   java -Xmx2g -jar ../../../rootbeer1/Rootbeer.jar HamaExamples.jar HamaExamples-GPU.jar
