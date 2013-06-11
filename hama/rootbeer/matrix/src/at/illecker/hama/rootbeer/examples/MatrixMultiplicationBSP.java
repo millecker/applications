f0fd7c777afeb97a18bf14655cceafd2dd252477
millecker <martin.illecker@gmaill.com>
2013-06-11 15:22:06 +0200

    Added Hama Rootbeer Matrix example

---
+++ hama/rootbeer/matrix/src/at/illecker/hama/rootbeer/examples/MatrixMultiplicationBSP.java
@@ -0,0 +1,13 @@
+package at.illecker.hama.rootbeer.examples;
+
+public class MatrixMultiplicationBSP {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
