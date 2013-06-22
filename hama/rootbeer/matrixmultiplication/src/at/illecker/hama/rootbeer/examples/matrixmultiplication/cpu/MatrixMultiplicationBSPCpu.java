aa6b072092f9ebc4031b47431a89259bd2eb765e
millecker <martin.illecker@gmail.com>
2013-06-22 18:14:32 +0200

    Fixed MatrixMultiplicationBSPCpu
    Added MatrixRowMessage

--- hama/rootbeer/matrixmultiplication/src/at/illecker/hama/rootbeer/examples/matrixmultiplication/cpu/MatrixMultiplicationBSPCpu.java
+++ hama/rootbeer/matrixmultiplication/src/at/illecker/hama/rootbeer/examples/matrixmultiplication/cpu/MatrixMultiplicationBSPCpu.java
@@ -23,12 +23,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hama.HamaConfiguration;
 import org.apache.hama.bsp.BSP;
 import org.apache.hama.bsp.BSPJob;
@@ -42,9 +42,11 @@ import org.apache.hama.bsp.join.CompositeInputFormat;
 import org.apache.hama.bsp.join.TupleWritable;
 import org.apache.hama.bsp.sync.SyncException;
 import org.apache.mahout.math.CardinalityException;
+import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.VectorWritable;
 
 import at.illecker.hama.rootbeer.examples.matrixmultiplication.util.DistributedRowMatrix;
+import at.illecker.hama.rootbeer.examples.matrixmultiplication.util.MatrixRowMessage;
 
 /**
  * @author MatrixMultiplication based on Mahout https://github.com/apache/mahout
@@ -53,48 +55,144 @@ import at.illecker.hama.rootbeer.examples.matrixmultiplication.util.DistributedR
  * 
  */
 
-public class MatrixMultiplicationBSPCpu extends
-    BSP<IntWritable, TupleWritable, IntWritable, VectorWritable, NullWritable> {
+public class MatrixMultiplicationBSPCpu
+    extends
+    BSP<IntWritable, TupleWritable, IntWritable, VectorWritable, MatrixRowMessage> {
 
   private static final Log LOG = LogFactory
       .getLog(MatrixMultiplicationBSPCpu.class);
 
-  private static final Path TMP_OUTPUT = new Path(
-      "output/hama/rootbeer/examples/matrixmultiplication/CPU-"
-          + System.currentTimeMillis());
   private static final String OUT_CARD = "output.vector.cardinality";
   private static final String DEBUG = "matrixmultiplication.bsp.cpu.debug";
 
   private static final Path OUTPUT_DIR = new Path(
-      "output/hadoop/rootbeer/examples/matrixmultiplication/CPU-"
+      "output/hama/rootbeer/examples/matrixmultiplication/CPU-"
           + System.currentTimeMillis());
 
   private static final Path MATRIX_A_PATH = new Path(
-      "input/hadoop/rootbeer/examples/MatrixA.seq");
+      "input/hama/rootbeer/examples/MatrixA.seq");
   private static final Path MATRIX_B_PATH = new Path(
-      "input/hadoop/rootbeer/examples/MatrixB.seq");
+      "input/hama/rootbeer/examples/MatrixB.seq");
   private static final Path MATRIX_C_PATH = new Path(OUTPUT_DIR
       + "/MatrixC.seq");
 
+  private int outCardinality;
+  private boolean isDebuggingEnabled;
+  private FSDataOutputStream logger;
+  private String masterTask;
+
   @Override
   public void setup(
-      BSPPeer<IntWritable, TupleWritable, IntWritable, VectorWritable, NullWritable> peer)
+      BSPPeer<IntWritable, TupleWritable, IntWritable, VectorWritable, MatrixRowMessage> peer)
       throws IOException {
+
+    Configuration conf = peer.getConfiguration();
+
+    outCardinality = conf.getInt(OUT_CARD, Integer.MAX_VALUE);
+    isDebuggingEnabled = conf.getBoolean(DEBUG, false);
+
+    // Choose one as a master, who sorts the matrix rows at the end
+    this.masterTask = peer.getPeerName(peer.getNumPeers() / 2);
+
+    // Init logging
+    if (isDebuggingEnabled) {
+      try {
+        FileSystem fs = FileSystem.get(conf);
+        logger = fs.create(new Path(FileOutputFormat.getOutputPath(
+            new BSPJob((HamaConfiguration) conf)).getParent()
+            + "/BSP_" + peer.getTaskId() + ".log"));
+
+        logger.writeChars("bsp,setup,outCardinality=" + outCardinality + "\n");
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
 
   }
 
   @Override
   public void bsp(
-      BSPPeer<IntWritable, TupleWritable, IntWritable, VectorWritable, NullWritable> peer)
+      BSPPeer<IntWritable, TupleWritable, IntWritable, VectorWritable, MatrixRowMessage> peer)
       throws IOException, SyncException, InterruptedException {
 
+    IntWritable key = new IntWritable();
+    TupleWritable value = new TupleWritable();
+    while (peer.readNext(key, value)) {
+
+      // Logging
+      if (isDebuggingEnabled) {
+        for (int i = 0; i < value.size(); i++) {
+          Vector vector = ((VectorWritable) value.get(i)).get();
+          logger.writeChars("bsp,input,key=" + key + ",value="
+              + vector.toString() + "\n");
+        }
+      }
+
+      Vector firstVector = ((VectorWritable) value.get(0)).get();
+      Vector secondVector = ((VectorWritable) value.get(1)).get();
+
+      // outCardinality is resulting column size n
+      // (l x m) * (m x n) = (l x n)
+      boolean firstIsOutFrag = secondVector.size() == outCardinality;
+
+      // outFrag is Matrix which has the resulting column cardinality
+      // (matrixB)
+      Vector outFrag = firstIsOutFrag ? secondVector : firstVector;
+
+      // multiplier is Matrix which has the resulting row count
+      // (transposed matrixA)
+      Vector multiplier = firstIsOutFrag ? firstVector : secondVector;
+
+      if (isDebuggingEnabled) {
+        logger.writeChars("bsp,firstIsOutFrag=" + firstIsOutFrag + "\n");
+        logger.writeChars("bsp,outFrag=" + outFrag + "\n");
+        logger.writeChars("bsp,multiplier=" + multiplier + "\n");
+      }
+
+      for (Vector.Element e : multiplier.nonZeroes()) {
+
+        VectorWritable outVector = new VectorWritable();
+        // Scalar Multiplication (Vector x Element)
+        outVector.set(outFrag.times(e.get()));
+
+        peer.send(masterTask, new MatrixRowMessage(e.index(), outVector));
+
+        if (isDebuggingEnabled) {
+          logger.writeChars("bsp,send,key=" + e.index() + ",value="
+              + outVector.get().toString() + "\n");
+        }
+      }
+      if (isDebuggingEnabled) {
+        logger.flush();
+      }
+    }
   }
 
   @Override
   public void cleanup(
-      BSPPeer<IntWritable, TupleWritable, IntWritable, VectorWritable, NullWritable> peer)
+      BSPPeer<IntWritable, TupleWritable, IntWritable, VectorWritable, MatrixRowMessage> peer)
       throws IOException {
 
+    /*
+     * Vector accumulator = new RandomAccessSparseVector(it.next().get()); while
+     * (it.hasNext()) { Vector row = it.next().get(); accumulator.assign(row,
+     * Functions.PLUS); }
+     */
+
+    if (peer.getPeerName().equals(masterTask)) {
+
+      MatrixRowMessage currentMatrixRowMessage = null;
+      while ((currentMatrixRowMessage = peer.getCurrentMessage()) != null) { //
+
+        if (isDebuggingEnabled) {
+          logger.writeChars("bsp,send,key="
+              + currentMatrixRowMessage.getRowIndex() + ",value="
+              + currentMatrixRowMessage.getRowValues().get().toString() + "\n");
+        }
+        peer.write(new IntWritable(currentMatrixRowMessage.getRowIndex()),
+            currentMatrixRowMessage.getRowValues());
+      }
+    }
   }
 
   static void printOutput(Configuration conf) throws IOException {
@@ -133,14 +231,14 @@ public class MatrixMultiplicationBSPCpu extends
     job.setJarByClass(MatrixMultiplicationBSPCpu.class);
 
     job.setInputFormat(CompositeInputFormat.class);
-    job.set("mapred.join.expr", CompositeInputFormat.compose("inner",
+    job.set("bsp.join.expr", CompositeInputFormat.compose("inner",
         SequenceFileInputFormat.class, aPath, bPath));
 
     job.setOutputFormat(SequenceFileOutputFormat.class);
     job.setOutputKeyClass(IntWritable.class);
     job.setOutputValueClass(VectorWritable.class);
 
-    FileOutputFormat.setOutputPath(job, TMP_OUTPUT);
+    FileOutputFormat.setOutputPath(job, OUTPUT_DIR);
 
     job.set(OUT_CARD, "" + outCardinality);
     job.set("bsp.child.java.opts", "-Xmx4G");
@@ -151,10 +249,10 @@ public class MatrixMultiplicationBSPCpu extends
   public static void main(String[] args) throws Exception {
 
     // Defaults
-    int numRowsA = 4;
-    int numColsA = 4;
-    int numRowsB = 4;
-    int numColsB = 4;
+    int numRowsA = 10;
+    int numColsA = 10;
+    int numRowsB = 10;
+    int numColsB = 10;
     boolean isDebugging = false;
 
     Configuration conf = new HamaConfiguration();
