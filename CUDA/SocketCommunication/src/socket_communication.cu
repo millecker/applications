19e2c9b1511fc7c0c3e436a976c43bd5feda6f1b
millecker <martin.illecker@gmail.com>
2013-07-23 11:03:07 +0200

    Add spinlock in device_method

--- CUDA/SocketCommunication/src/socket_communication.cu
+++ CUDA/SocketCommunication/src/socket_communication.cu
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include <signal.h>
 #include <pthread.h>
@@ -47,8 +48,10 @@ private:
 public:
 	volatile MESSAGE_TYPE command;
 	volatile bool has_task;
-	volatile int active_thread_id;
+	volatile bool task_accepted;
+	volatile int lock_thread_id;
 	volatile bool done;
+	volatile bool is_monitoring;
 
 	KernelWrapper() {
 		init();
@@ -56,31 +59,42 @@ public:
 	~KernelWrapper() {
 	}
 
+	void init() {
+		printf("KernelWrapper init\n");
+
+		is_monitoring = false;
+		done = false;
+		//pthread_mutex_init(&mutex_has_task, NULL);
+
+		reset();
+	}
+
 	void startMonitor() {
+		printf("KernelWrapper startMonitor\n");
 		pthread_create(&t_monitor, NULL, &KernelWrapper::thread, this);
 	}
 
 	static void *thread(void *context) {
-		volatile KernelWrapper *_this = ((KernelWrapper *) context);
+		KernelWrapper *_this = ((KernelWrapper *) context);
 
 		while (!_this->done) {
+			_this->is_monitoring = true;
 			//printf(
-			//		"MonitorThread has_task: %s, active_thread_id: %d, command: %d\n",
+			//		"KernelWrapper MonitorThread has_task: %s, task_accepted: %s, lock_thread_id: %d, command: %d\n",
 			//		(_this->has_task) ? "true" : "false",
-			//		_this->active_thread_id, _this->command);
+			//		(_this->task_accepted) ? "true" : "false",
+			//		_this->lock_thread_id, _this->command);
 
-			if ((!_this->has_task) && (_this->active_thread_id >= 0)
+			if ((!_this->has_task) && (_this->lock_thread_id >= 0)
 					&& (_this->command != UNDEFINED)) {
-				_this->has_task = true;
+				_this->task_accepted = true;
 				_this->sendCommand();
 			}
 		}
 		return NULL;
 	}
 
-	void sendCommand() volatile {
-
-		printf("MonitorThread sendCommand!\n");
+	void sendCommand() {
 
 		switch (command) {
 
@@ -96,41 +110,64 @@ public:
 		}
 		case DONE: {
 			socket_client.sendCMD(DONE);
+			result_available = true;
 			break;
 		}
 
 		}
 	}
 
-	__device__ __host__ void init() {
-		active_thread_id = -1;
+	__device__ __host__ void reset() {
+		lock_thread_id = -1;
 		command = UNDEFINED;
 		has_task = false;
+		task_accepted = false;
 		result_available = false;
-		done = false;
 	}
 
-	__device__ int getValue(int val) {
+	__device__ __host__ int getValue(int val) {
+
+		// wait for wrapper thread to end
+		while (has_task) {
+		}
 
 		command = GET_VALUE;
 		param1 = val;
+		has_task = true;
 		//__threadfence_system();
 
-		// wait for socket communication
-		while (true) {
-			if (result_available) {
-				break;
-			}
+		// wait for wrapper thread to accept
+		while (!task_accepted) {
 		}
 
-		init();
+		// wait for socket communication to end
+		while (!result_available) {
+		}
+
+		reset();
 		return result_int;
 	}
 
 	__device__ __host__ void sendDone() {
 		command = DONE;
+		has_task = true;
+
+		// wait for wrapper thread to accept
+		while (!task_accepted) {
+		}
+
+		printf(
+				"KernelWrapper::sendDone: has_task: %s, task_accepted: %s, lock_thread_id: %d, command: %d\n",
+				(has_task) ? "true" : "false",
+				(task_accepted) ? "true" : "false", lock_thread_id, command);
+
+		// wait for socket communication to end
+		while (!result_available) {
+		}
+
+		reset();
 		done = true;
-		//init();
+		return;
 	}
 };
 
@@ -139,48 +176,83 @@ KernelWrapper *h_kernelWrapper;
 void sigint_handler(int s) {
 	printf("Caught signal %d\n", s);
 
-	h_kernelWrapper->active_thread_id = 0;
-	h_kernelWrapper->sendDone();
+	if (h_kernelWrapper->is_monitoring) {
+		h_kernelWrapper->lock_thread_id = 0;
+		h_kernelWrapper->sendDone();
 
-	pthread_join(t_socket_server, NULL);
+		pthread_join(t_socket_server, NULL);
+	}
 	exit(0);
 }
 
 // Convenience function for checking CUDA runtime API results
 // can be wrapped around any runtime API call. No-op in release builds.
 inline cudaError_t checkCuda(cudaError_t result) {
-#if defined(DEBUG) || defined(_DEBUG)
 	if (result != cudaSuccess) {
 		fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
 		assert(result == cudaSuccess);
 	}
-#endif
 	return result;
 }
 
 __global__ void device_method(KernelWrapper *d_kernelWrapper) {
 
 	int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
-	bool accessed = false;
 
-	// getValue for each Thread
-	do {
+	int old;
+	int count = 0;
 
-		atomicExch((int *) &d_kernelWrapper->active_thread_id, thread_id);
-		//__threadfence_system();
+	while (count < 100) {
+		old = atomicCAS((int *) &d_kernelWrapper->lock_thread_id, -1,
+				thread_id);
+		if (old == -1 || old == thread_id) {
+			//do critical section code
 
-		if (d_kernelWrapper->active_thread_id == thread_id) {
-			accessed = true;
 			cuPrintf("Thread %d active_thread_id: %d\n", thread_id,
-					d_kernelWrapper->active_thread_id);
+					d_kernelWrapper->lock_thread_id);
 
 			int val = d_kernelWrapper->getValue(thread_id);
-			cuPrintf("Thread %d getValue: %d\n", thread_id, val);
+
+		} else {
+			count++;
+			if (count > 50) {
+				count = 0;
+			}
 		}
+	}
 
-		syncthreads();
+	/*
+	 bool accessed = false;
 
-	} while (!accessed);
+	 // getValue for each Thread
+	 do {
+
+	 // wait for possible old task
+	 while (d_kernelWrapper->has_task) {
+	 }
+
+	 atomicExch((int *) &d_kernelWrapper->active_thread_id, thread_id);
+	 //__threadfence_system();
+
+	 if (d_kernelWrapper->active_thread_id == thread_id) {
+	 accessed = true;
+	 cuPrintf("Thread %d active_thread_id: %d\n", thread_id,
+	 d_kernelWrapper->active_thread_id);
+
+	 cuPrintf("Thread %d task_accepted: %s\n", thread_id,
+	 (d_kernelWrapper->task_accepted) ? "true" : "false");
+
+	 d_kernelWrapper->has_task = true;
+	 int val = d_kernelWrapper->getValue(thread_id);
+	 d_kernelWrapper->has_task = false;
+
+	 cuPrintf("Thread %d getValue: %d\n", thread_id, val);
+	 }
+
+	 syncthreads();
+
+	 } while (!accessed);
+	 */
 }
 
 int main(void) {
@@ -216,6 +288,12 @@ int main(void) {
 	h_kernelWrapper->init();
 	h_kernelWrapper->startMonitor();
 
+	// wait for kernelWrapper monitoring
+	while (!h_kernelWrapper->is_monitoring) {
+	}
+	printf("KernelWrapper is_monitoring: %s\n",
+			(h_kernelWrapper->is_monitoring) ? "true" : "false");
+
 	// test call host_kernelWrapper
 	//h_kernelWrapper->active_thread_id = 0;
 	//int value = h_kernelWrapper->getValue(0);
@@ -227,14 +305,14 @@ int main(void) {
 	// initialize cuPrintf
 	cudaPrintfInit();
 
-	device_method<<<4, 1>>>(d_kernelWrapper);
+	//device_method<<<4, 1>>>(d_kernelWrapper);
 
 	// display the device's output
 	cudaPrintfDisplay();
 	// clean up after cuPrintf
 	cudaPrintfEnd();
 
-	h_kernelWrapper->active_thread_id = 0;
+	h_kernelWrapper->lock_thread_id = 0;
 	h_kernelWrapper->sendDone();
 
 	// wait for SocketServer
