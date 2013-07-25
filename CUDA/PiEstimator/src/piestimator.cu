ab55833ba6f24ff90d3048456fc2f2b2d0ed21a5
millecker <martin.illecker@gmail.com>
2013-07-25 13:57:53 +0200

    Added PiEstimator CUDA example

---
+++ CUDA/PiEstimator/src/piestimator.cu
@@ -0,0 +1,158 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+#include <cuda_runtime.h>
+
+// Convenience function for checking CUDA runtime API results
+// can be wrapped around any runtime API call. No-op in release builds.
+inline cudaError_t checkCuda(cudaError_t result) {
+	if (result != cudaSuccess) {
+		fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+		assert(result == cudaSuccess);
+	}
+	return result;
+}
+
+/**
+ * Device function
+ */__global__ void device_method(int n, float *d_inside, float *d_x, float *d_y) {
+
+	unsigned block_id = blockIdx.y * gridDim.x + blockIdx.x;
+	int thread_id = threadIdx.x + block_id * blockDim.x;
+
+	if (thread_id < n) {
+		float x = d_x[thread_id];
+		float y = d_y[thread_id];
+
+		if (sqrt(x * x + y * y) <= 1) {
+			d_inside[thread_id] = 1;
+		} else {
+			d_inside[thread_id] = 0;
+		}
+	}
+}
+
+int divup(int x, int y) {
+	if (x % y != 0) {
+		// aufrunden
+		return ((x + y - 1) / y);
+	} else {
+		return x / y;
+	}
+}
+
+float rand_float_range(float low, float high) {
+	return low + (((float) rand() / RAND_MAX) * (high - low));
+}
+
+// not a uniform distribution
+void random_number_generator(float* array, int size) {
+	for (int i = 0; i < size; i++) {
+		array[i] = 2 * rand_float_range(0, 1) - 1;
+	}
+}
+
+void vector_sum(float* result, float* array, int size) {
+	for (int i = 0; i < size; i++) {
+		*result += array[i];
+	}
+}
+
+/**
+ * Host function
+ */
+int main(void) {
+
+	// Fetch available GPU memory size to fix the samples count.
+	cudaDeviceProp devProp;
+	checkCuda(cudaGetDeviceProperties(&devProp, 0));
+
+	unsigned maxbytes = devProp.totalGlobalMem / 3;
+	unsigned max_samples = maxbytes / sizeof(float);
+	// Does GPU support 2e6 samples?
+	int n = 2e6;
+	if (n > max_samples) {
+		n = max_samples;
+	}
+	printf("Using %d samples to estimate pi\n", n);
+
+	// random scattering of (x,y) points
+	float *h_x = NULL;
+	float *h_y = NULL;
+	float *h_inside = NULL;
+	unsigned bytes = sizeof(float) * n;
+
+	// allocate memory
+	checkCuda(cudaSetDeviceFlags(cudaDeviceMapHost));
+	checkCuda(
+			cudaHostAlloc(&h_x, bytes,
+					cudaHostAllocWriteCombined | cudaHostAllocMapped));
+	checkCuda(
+			cudaHostAlloc(&h_y, bytes,
+					cudaHostAllocWriteCombined | cudaHostAllocMapped));
+	checkCuda(
+			cudaHostAlloc(&h_inside, bytes,
+					cudaHostAllocWriteCombined | cudaHostAllocMapped));
+
+	// fill with random numbers
+	srand((unsigned) time(0));
+	random_number_generator(h_x, n);
+	random_number_generator(h_y, n);
+	printf("Filled x and y float array with random numbers\n");
+
+	// setup GPU parameters
+	dim3 threads(256);
+	dim3 blocks(divup(n, threads.x));
+	if (blocks.x > 65535) {
+		blocks.y = divup(blocks.x, 65535);
+		blocks.x = divup(blocks.x, blocks.y);
+	}
+
+	float *d_x = NULL;
+	float *d_y = NULL;
+	float *d_inside = NULL;
+	checkCuda(cudaHostGetDevicePointer(&d_x, h_x, 0));
+	checkCuda(cudaHostGetDevicePointer(&d_y, h_y, 0));
+	checkCuda(cudaHostGetDevicePointer(&d_inside, h_inside, 0));
+
+	printf("Run computation on GPU\n");
+	device_method<<<blocks, threads>>>(n, d_inside, d_x, d_y);
+
+#ifdef DEBUG // For debugging purposes
+// Check for CUDA runtime errors
+	CUDA(cudaDeviceSynchronize());
+	CUDA(cudaGetLastError());
+#endif
+
+	// count how many fell inside
+	float h_result;
+	vector_sum(&h_result, h_inside, n);
+
+	// approximate PI
+	float pi = 4.0f * h_result / n;
+	printf("pi: %f\n", pi);
+
+	checkCuda(cudaFreeHost(h_x));
+	checkCuda(cudaFreeHost(h_y));
+	checkCuda(cudaFreeHost(h_inside));
+
+	return 0;
+}
