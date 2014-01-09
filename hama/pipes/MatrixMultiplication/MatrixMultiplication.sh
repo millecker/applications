812486f94c14e9c678b820196591d17ddb5673ef
millecker <martin.illecker@gmail.com>
2014-01-09 13:15:28 +0100

    Fix Hama Pipes MatrixMultiplication example

--- hama/pipes/MatrixMultiplication/MatrixMultiplication.sh
+++ hama/pipes/MatrixMultiplication/MatrixMultiplication.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-hadoop fs -rmr output/matrixmult
-hadoop fs -rmr bin/cpu_MatrixMultiplication
+clear
 
+# Delete hdfs directories
+hadoop fs -rmr /examples/input/matrixmultiplication
+hadoop fs -rmr /examples/output/matrixmultiplication
+hadoop fs -rmr /examples/bin/cpu_matrixmultiplication
+
+# Generate input data
+hama jar $HAMA_HOME/hama-examples-*.jar \
+  gen vectorwritablematrix 4 4 \
+  /examples/input/matrixmultiplication/MatrixA.seq \
+  false true 0 10 0
+
+hama jar $HAMA_HOME/hama-examples-*.jar \
+  gen vectorwritablematrix 4 4 \
+  /examples/input/matrixmultiplication/MatrixB_transposed.seq \
+  false true 0 10 0
+
+# Compile and upload binary
 cd cpu-MatrixMultiplication
 make clean && make
+hadoop fs -put MatrixMultiplication /examples/bin/cpu_matrixmultiplication
 cd ..
 
-hadoop fs -put cpu-MatrixMultiplication/MatrixMultiplication bin/cpu_MatrixMultiplication
+# Run matrixmultiplication example
+hama pipes -conf MatrixMultiplication_job.xml \
+ -output /examples/output/matrixmultiplication
 
-hadoop fs -rmr input/matrixmult
-hadoop fs -mkdir input/matrixmult
-hadoop fs -put input/* input/matrixmult
-
-hama pipes -conf MatrixMultiplication_job.xml -output output/matrixmult
-
-hama seqdumper -seqFile input/matrixmult/matrixA_10x10.seq
-
-hama seqdumper -seqFile input/matrixmult/transposedMatrixB_10x10.seq
-
-hama seqdumper -seqFile output/matrixmult/part-00001
+# Print output
+hama seqdumper -file /examples/input/matrixmultiplication/MatrixA.seq
+hama seqdumper -file /examples/input/matrixmultiplication/MatrixB_transposed.seq
+hama seqdumper -file /examples/output/matrixmultiplication/part-00001
